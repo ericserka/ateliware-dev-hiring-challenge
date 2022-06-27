68d596a06ea8c3816de57eb82afa5e6c2a104fda
ericserka <ericserka@hotmail.com>
2022-06-27 15:32:41 -0300

    feat: prisma ORM and CRUD service

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,42 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Repository as RepositoryModel,
+  User as UserModel,
+} from '@prisma/client';
 import { AppService } from './app.service';
 
+// TODO: implementar DTOs nos controllers
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('repository/:id')
+  async getRepositoryById(@Param('id') id: string): Promise<RepositoryModel> {
+    return this.appService.repository({ id: Number(id) });
+  }
+
+  @Get('repositories')
+  async getRepositories(
+    @Param('userId') userId: string,
+  ): Promise<RepositoryModel[]> {
+    return this.appService.repositories({
+      where: { userId },
+    });
+  }
+
+  @Post('repository')
+  async createRepository(
+    @Body() repositoryData: { dataObj: object; userId: string },
+  ): Promise<RepositoryModel> {
+    const { dataObj, userId } = repositoryData;
+    return this.appService.createRepository({
+      dataObj,
+      User: { connect: { id: userId } },
+    });
+  }
+
+  @Post('user')
+  async createUser(@Body() userData: { id: string }): Promise<UserModel> {
+    return this.appService.createUser(userData);
   }
 }
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,72 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma, Repository, User } from '@prisma/client';
+import { PrismaService } from './prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private prisma: PrismaService) {}
+
+  async user(
+    userWhereUniqueInput: Prisma.UserWhereUniqueInput,
+  ): Promise<User | null> {
+    return this.prisma.user.findUnique({
+      where: userWhereUniqueInput,
+    });
+  }
+
+  async users(params: {
+    skip?: number;
+    take?: number;
+    cursor?: Prisma.UserWhereUniqueInput;
+    where?: Prisma.UserWhereInput;
+    orderBy?: Prisma.UserOrderByWithRelationInput;
+  }): Promise<User[]> {
+    const { skip, take, cursor, where, orderBy } = params;
+    return this.prisma.user.findMany({
+      skip,
+      take,
+      cursor,
+      where,
+      orderBy,
+    });
+  }
+
+  async createUser(data: Prisma.UserCreateInput): Promise<User> {
+    return this.prisma.user.create({
+      data,
+    });
+  }
+
+  async repository(
+    repositoryWhereUniqueInput: Prisma.RepositoryWhereUniqueInput,
+  ): Promise<Repository | null> {
+    return this.prisma.repository.findUnique({
+      where: repositoryWhereUniqueInput,
+    });
+  }
+
+  async repositories(params: {
+    skip?: number;
+    take?: number;
+    cursor?: Prisma.RepositoryWhereUniqueInput;
+    where?: Prisma.RepositoryWhereInput;
+    orderBy?: Prisma.RepositoryOrderByWithRelationInput;
+  }): Promise<Repository[]> {
+    const { skip, take, cursor, where, orderBy } = params;
+    return this.prisma.repository.findMany({
+      skip,
+      take,
+      cursor,
+      where,
+      orderBy,
+    });
+  }
+
+  async createRepository(
+    data: Prisma.RepositoryCreateInput,
+  ): Promise<Repository> {
+    return this.prisma.repository.create({
+      data,
+    });
   }
 }
